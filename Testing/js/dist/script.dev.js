4c1dc53664016f820a3ab9090ef8834fade335c1
Yura <yuriy.motsnyy@gmail.com>
2022-12-29 02:04:28 +0200

    added object personalMovieDB

--- Testing/js/dist/script.dev.js
+++ Testing/js/dist/script.dev.js
@@ -1,1 +1,14 @@
 "use strict";
+
+var numberOfFilms = +prompt("Скільки фільмів Ви вже подивились?", "");
+var personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+console.log(personalMovieDB);
+var lastWatchedMovie = prompt("Один з останніх переглянутих фільмів", "");
+var estimateMovie = prompt("На скільки оціните його", "");
+personalMovieDB.movies[lastWatchedMovie] = estimateMovie;
--- Testing/js/script.js
+++ Testing/js/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const numberOfFilms = +prompt("Скільки фільмів Ви вже подивились?", "");
+
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+console.log(personalMovieDB);
+
+const lastWatchedMovie = prompt("Один з останніх переглянутих фільмів", "");
+const estimateMovie = prompt("На скільки оціните його", "");
+
+
+personalMovieDB.movies[lastWatchedMovie] = estimateMovie; 
